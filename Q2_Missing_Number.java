113de266851f93e19b4b526f7f39797e89158eb0
SOMNATH0904 <somnathshaw900@gmail.com>
2023-10-14 09:58:32 +0530

    Q2-Missing-Number

---
+++ Q2_Missing_Number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q2_Missing_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long sum = (n*(n+1))/2;
+        for(int i = 0; i < n - 1; i++) {
+            long temp = sc.nextLong();
+            sum = sum - temp;
+        }
+        System.out.println(sum);
+    }
+}
