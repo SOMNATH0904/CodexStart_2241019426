01e84f165b4258affb4d987c3335c8fa85860ddd
SOMNATH0904 <somnathshaw900@gmail.com>
2023-10-16 10:11:11 +0530

    Q3_1069

--- Q3_1069.java
+++ Q3_1069.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;
 
-public class Repetitions {
+public class Q3_1069 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
