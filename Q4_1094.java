3e1352d8f18c69ca04e8a1fac01fa9f70958f56d
SOMNATH0904 <somnathshaw900@gmail.com>
2023-10-16 10:10:28 +0530

    Q4_1094

---
+++ Q4_1094.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q4_1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        int[] arr = new int[(int) n];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        long sum = 0;
+        for(int i = 1; i < n; i++) {
+            if(arr[i] < arr[i-1]) {
+                sum += Math.abs(arr[i] - arr[i-1]);
+                arr[i] = arr[i-1];
+            }
+        }
+        System.out.println(sum);
+    }
+}
